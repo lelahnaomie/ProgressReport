1441b69f81cf0f92f6d30920159e79bc1b9cd40b
lelahnaomie <124450076+lelahnaomie@users.noreply.github.com>
2026-01-22 16:27:38 +0100

    login modification

--- api/login.js
+++ api/login.js
@@ -1,6 +1,7 @@
 import { createClient } from "@libsql/client";
 
 export default async function handler(req, res) {
+    console.log("Login attempt for:", email);
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
